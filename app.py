c5e4199f6a6c3938ae5ba8d332558fd4c9209aca
thunguyenbizch <37186404-thunguyenbizch@users.noreply.replit.com>
2024-10-17 12:55:29 +0000

    Checkpoint before revert - Investigate and fix the subscription checkout process

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
-
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
-login_manager = LoginManager()
-migrate = Migrate()
+from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -19,17 +12,23 @@ app.config.from_object('config.Config')
 app.config['STRIPE_PUBLIC_KEY'] = os.environ.get('STRIPE_PUBLIC_KEY')
 app.config['STRIPE_SECRET_KEY'] = os.environ.get('STRIPE_SECRET_KEY')
 app.config['STRIPE_WEBHOOK_SECRET'] = os.environ.get('STRIPE_WEBHOOK_SECRET')
-app.config['STRIPE_PRICE_ID'] = os.environ.get('STRIPE_PRICE_ID')  # Add this line
+app.config['STRIPE_PRICE_ID'] = os.environ.get('STRIPE_PRICE_ID')
 
-db.init_app(app)
-login_manager.init_app(app)
-migrate.init_app(app, db)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 # Configure upload folder
 app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
+
+from models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 with app.app_context():
     import models
